48ce313975d78021f4dcdfac60e2895a5079e955
Stefan Siegler <stefan.siegler@stud.hslu.ch>
2024-01-20 00:49:24 +0100

    advanced env + analysys

--- gym_mtsim/__init__.py
+++ gym_mtsim/__init__.py
@@ -3,6 +3,7 @@ from gym.envs.registration import register
 from .metatrader import Timeframe, SymbolInfo
 from .simulator import MtSimulator, OrderType, Order, SymbolNotFound, OrderNotFound
 from .envs import MtEnv
+from .envs import MtEnvAdvanced
 from .data import FOREX_DATA_PATH, STOCKS_DATA_PATH, CRYPTO_DATA_PATH, MIXED_DATA_PATH
 
 
--- gym_mtsim/envs/__init__.py
+++ gym_mtsim/envs/__init__.py
@@ -1,1 +1,2 @@
 from .mt_env import MtEnv
+from .mt_env_advanced import MtEnvAdvanced
